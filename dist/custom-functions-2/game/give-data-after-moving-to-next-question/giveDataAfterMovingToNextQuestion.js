1c002aa5956e5bd61f2c463f06010562ee90e487
Apurbo Roy <apurboroy7077@gmail.com>
2024-10-16 21:36:02 +0600

    version 1 prototype

--- dist/custom-functions-2/game/give-data-after-moving-to-next-question/giveDataAfterMovingToNextQuestion.js
+++ dist/custom-functions-2/game/give-data-after-moving-to-next-question/giveDataAfterMovingToNextQuestion.js
@@ -8,6 +8,7 @@ export const giveDataAfterMovingToNextQuestion = (data) => {
                 const allQuestionsData = singleData.allQuestions;
                 let doesAllUserHasAnsweredCurrentQuestion = false;
                 let numberOfUserWhoAnsweredCurrentQuestion = 0;
+                let does3secondsPassedSinceUserAnsweredLastQuestion = false;
                 for (let i = 0; i < usersData.length; i++) {
                     const singleUserData = usersData[i];
                     const answeredQuestionData = singleUserData.answeredQuestion;
@@ -19,18 +20,41 @@ export const giveDataAfterMovingToNextQuestion = (data) => {
                         }
                     }
                 }
+                // If all user answered the current question
                 if (numberOfUserWhoAnsweredCurrentQuestion >= usersData.length) {
-                    let currentQuestionIndex = 0;
-                    for (let i = 0; i < allQuestionsData.length; i++) {
-                        const singleQuestion = allQuestionsData[i].question;
-                        if (singleQuestion === currentQuestion) {
-                            currentQuestionIndex = i;
+                    const lastAnsweredQuestionOfEveryUser = [];
+                    for (let i = 0; i < usersData.length; i++) {
+                        const singleUserData = usersData[i];
+                        const answeredQuestionData = singleUserData.answeredQuestion;
+                        const lastQuestion = answeredQuestionData[answeredQuestionData.length - 1];
+                        lastAnsweredQuestionOfEveryUser.push(lastQuestion);
+                    }
+                    let does3SecondsPassedSinceEveryoneAnsweredLastQuestion = true;
+                    for (let i = 0; i < lastAnsweredQuestionOfEveryUser.length; i++) {
+                        const singleData = lastAnsweredQuestionOfEveryUser[i];
+                        const timeStamp = singleData.timeStamp;
+                        const timeToCross = timeStamp + 3000;
+                        const currentTime = Date.now();
+                        if (timeToCross > currentTime) {
+                            does3SecondsPassedSinceEveryoneAnsweredLastQuestion = false;
                         }
                     }
-                    const nextQuestionIndex = currentQuestionIndex + 1;
-                    const nextQuestion = allQuestionsData[nextQuestionIndex];
-                    if (nextQuestion) {
-                        singleData.currentQuestionData = nextQuestion;
+                    if (does3SecondsPassedSinceEveryoneAnsweredLastQuestion) {
+                        let currentQuestionIndex = 0;
+                        for (let i = 0; i < allQuestionsData.length; i++) {
+                            const singleQuestion = allQuestionsData[i].question;
+                            if (singleQuestion === currentQuestion) {
+                                currentQuestionIndex = i;
+                            }
+                        }
+                        const nextQuestionIndex = currentQuestionIndex + 1;
+                        const nextQuestion = allQuestionsData[nextQuestionIndex];
+                        if (nextQuestion) {
+                            singleData.currentQuestionData = nextQuestion;
+                        }
+                        else {
+                            singleData.gameRunningStatus = "GAME_OVER";
+                        }
                     }
                 }
             }
--- src/custom-functions-2/game/give-data-after-moving-to-next-question/giveDataAfterMovingToNextQuestion.ts
+++ src/custom-functions-2/game/give-data-after-moving-to-next-question/giveDataAfterMovingToNextQuestion.ts
@@ -9,6 +9,7 @@ export const giveDataAfterMovingToNextQuestion = (data: any) => {
 
         let doesAllUserHasAnsweredCurrentQuestion = false;
         let numberOfUserWhoAnsweredCurrentQuestion = 0;
+        let does3secondsPassedSinceUserAnsweredLastQuestion = false;
         for (let i = 0; i < usersData.length; i++) {
           const singleUserData = usersData[i];
           const answeredQuestionData = singleUserData.answeredQuestion;
@@ -20,19 +21,43 @@ export const giveDataAfterMovingToNextQuestion = (data: any) => {
             }
           }
         }
+        // If all user answered the current question
 
         if (numberOfUserWhoAnsweredCurrentQuestion >= usersData.length) {
-          let currentQuestionIndex = 0;
-          for (let i = 0; i < allQuestionsData.length; i++) {
-            const singleQuestion = allQuestionsData[i].question;
-            if (singleQuestion === currentQuestion) {
-              currentQuestionIndex = i;
+          const lastAnsweredQuestionOfEveryUser: any = [];
+          for (let i = 0; i < usersData.length; i++) {
+            const singleUserData = usersData[i];
+            const answeredQuestionData = singleUserData.answeredQuestion;
+            const lastQuestion =
+              answeredQuestionData[answeredQuestionData.length - 1];
+            lastAnsweredQuestionOfEveryUser.push(lastQuestion);
+          }
+          let does3SecondsPassedSinceEveryoneAnsweredLastQuestion = true;
+          for (let i = 0; i < lastAnsweredQuestionOfEveryUser.length; i++) {
+            const singleData = lastAnsweredQuestionOfEveryUser[i];
+            const timeStamp = singleData.timeStamp;
+            const timeToCross = timeStamp + 3000;
+            const currentTime = Date.now();
+            if (timeToCross > currentTime) {
+              does3SecondsPassedSinceEveryoneAnsweredLastQuestion = false;
             }
           }
-          const nextQuestionIndex = currentQuestionIndex + 1;
-          const nextQuestion = allQuestionsData[nextQuestionIndex];
-          if (nextQuestion) {
-            singleData.currentQuestionData = nextQuestion;
+
+          if (does3SecondsPassedSinceEveryoneAnsweredLastQuestion) {
+            let currentQuestionIndex = 0;
+            for (let i = 0; i < allQuestionsData.length; i++) {
+              const singleQuestion = allQuestionsData[i].question;
+              if (singleQuestion === currentQuestion) {
+                currentQuestionIndex = i;
+              }
+            }
+            const nextQuestionIndex = currentQuestionIndex + 1;
+            const nextQuestion = allQuestionsData[nextQuestionIndex];
+            if (nextQuestion) {
+              singleData.currentQuestionData = nextQuestion;
+            } else {
+              singleData.gameRunningStatus = "GAME_OVER";
+            }
           }
         }
       }
